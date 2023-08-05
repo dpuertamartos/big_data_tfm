82763fd66528a8c159ebea8dd0c1b7cae5eb84c8
David Puerta Martos <dpuerta@usal.es>
2023-08-05 15:54:56 +0200

    primary database is now mongodb

--- ingestion_scrapper/ingestion_scrapper/spiders/example_spider.py
+++ ingestion_scrapper/ingestion_scrapper/spiders/example_spider.py
@@ -1,25 +1,39 @@
 import scrapy
-from .. import database
+import pymongo
 
 
 class PisosSpider(scrapy.Spider):
     name = 'general'
-    cities = ['jaen']  # Ciudades a crawlear
+    cities = ['a_coruna', 'alava_araba', 'albacete', 'alicante', 'almeria', 'andorra', 'asturias', 'avila',
+              'badajoz', 'islas_baleares_illes_balears', 'barcelona', 'bilbao', 'burgos', 'caceres',
+              'cadiz', 'cantabria', 'castellon_castello', 'pisos-cerdanya_francesa', 'ceuta', 'ciudad_real', 'cordoba',
+              'cuenca', 'el_hierro', 'formentera', 'fuerteventura', 'gijon_concejo_xixon_conceyu_gijon',
+              'guipuzcoa_gipuzkoa', 'girona', 'gran_canaria', 'granada', 'guadalajara', 'huelva', 'huesca', 'isla_de_ibiza_eivissa'
+              'jaen', 'la_palma', 'la_rioja', 'lanzarote', 'las_palmas', 'las_palmas_de_gran_canaria', 'leon',
+              'lleida', 'logrono', 'lugo', 'madrid', 'malaga', 'isla_de_mallorca', 'melilla', 'isla_de_menorca', 'murcia',
+              'navarra_nafarroa', 'oviedo', 'pais_vasco_frances_iparralde', 'palencia', 'isla_de_mallorca_palma_de_mallorca',
+              'pamplona_iruna', 'pontevedra', 'salamanca', 'san_sebastian_donostia',
+              'santander', 'segovia', 'sevilla', 'soria', 'tarragona', 'tenerife',
+              'teruel', 'toledo', 'valencia', 'valladolid', 'vigo', 'vitoria_gasteiz_zona_urbana', 'vizcaya_bizkaia', 'zamora', 'zaragoza']
+    # Ciudades a crawlear
     start_urls = ['https://www.pisos.com/venta/pisos-{}/fecharecientedesde-desc/1/'.format(city) for city in cities]
 
     def __init__(self):
         self.max_page_to_search = 100
-        self.db_conn = database.connect_db('pisos.db')
-        database.create_table(self.db_conn)
+        self.client = pymongo.MongoClient("mongodb://localhost:27017/")  # Adjust the connection string if needed
+        self.db = self.client["pisos"]  # Change to your database name
+        self.collection = self.db["pisos_ingestion"]  # Change to your collection name
 
     def parse(self, response):
         if response.status in range(300, 310):
             return
 
         city = response.url.split('-')[1].split('/')[0]
+        collection = self.db[city]  # Use the city's name as the collection name
+
         for ad in response.css('div.ad-preview'):
             id = ad.xpath('@id').get()
-            if not database.check_id_exists(self.db_conn, id):
+            if not collection.find_one({"id": id}):  # Use the city's specific collection
                 data = {
                     'id': id,
                     'title': ad.css('a.ad-preview__title::text').get(),
@@ -35,7 +49,7 @@ class PisosSpider(scrapy.Spider):
                     'link': response.urljoin(ad.css('a.ad-preview__title::attr(href)').get()),
                 }
 
-                database.insert_data(self.db_conn, data)
+                collection.insert_one(data)  # Insert data into the city's specific collection
                 print("saved data=", data)
 
         next_page_number = int(response.url.split('/')[-2]) + 1
@@ -47,5 +61,5 @@ class PisosSpider(scrapy.Spider):
             yield scrapy.Request(next_page_url, callback=self.parse)
 
     def close_spider(self, spider):
-        self.db_conn.close()
+        self.client.close()
 
