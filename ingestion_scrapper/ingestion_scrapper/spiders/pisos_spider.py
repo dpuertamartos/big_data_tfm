01096d731ce23dd23d7aa28b37b67b134c4e5e59
David Puerta Martos <dpuerta@usal.es>
2023-08-08 19:58:38 +0200

    update mode now updates most recent date in database and for coincident date checks if the item is already in the collection to not duplicate it

--- ingestion_scrapper/ingestion_scrapper/spiders/pisos_spider.py
+++ ingestion_scrapper/ingestion_scrapper/spiders/pisos_spider.py
@@ -2,46 +2,51 @@ import scrapy
 import pymongo
 import datetime
 
+OLD_DATE = "Anuncio 15/10/1991"
+
 
 class PisosSpider(scrapy.Spider):
     name = 'general'
 
     cities = ['a_coruna', 'alava_araba', 'albacete', 'alicante', 'almeria', 'andorra', 'asturias', 'avila',
-              'badajoz', 'islas_baleares_illes_balears', 'barcelona', 'bilbao', 'burgos', 'caceres',
-              'cadiz', 'cantabria', 'castellon_castello', 'pisos-cerdanya_francesa', 'ceuta', 'ciudad_real', 'cordoba',
+              'badajoz', 'barcelona', 'bilbao', 'burgos', 'caceres',
+              'cadiz', 'cantabria', 'castellon_castello', 'cerdanya_francesa', 'ceuta', 'ciudad_real', 'cordoba',
               'cuenca', 'el_hierro', 'formentera', 'fuerteventura', 'gijon_concejo_xixon_conceyu_gijon',
-              'guipuzcoa_gipuzkoa', 'girona', 'gran_canaria', 'granada', 'guadalajara', 'huelva', 'huesca', 'isla_de_ibiza_eivissa'
-              'jaen', 'la_palma', 'la_rioja', 'lanzarote', 'las_palmas', 'las_palmas_de_gran_canaria', 'leon',
+              'guipuzcoa_gipuzkoa', 'girona', 'granada', 'guadalajara', 'huelva', 'huesca', 'isla_de_ibiza_eivissa',
+              'jaen', 'la_palma', 'la_rioja', 'lanzarote', 'las_palmas', 'leon',
               'lleida', 'logrono', 'lugo', 'madrid', 'malaga', 'isla_de_mallorca', 'melilla', 'isla_de_menorca', 'murcia',
-              'navarra_nafarroa', 'oviedo', 'pais_vasco_frances_iparralde', 'palencia', 'isla_de_mallorca_palma_de_mallorca',
+              'navarra_nafarroa', 'oviedo', 'pais_vasco_frances_iparralde', 'palencia',
               'pamplona_iruna', 'pontevedra', 'salamanca', 'san_sebastian_donostia',
               'santander', 'segovia', 'sevilla', 'soria', 'tarragona', 'tenerife',
               'teruel', 'toledo', 'valencia', 'valladolid', 'vigo', 'vitoria_gasteiz_zona_urbana', 'vizcaya_bizkaia', 'zamora', 'zaragoza']
+
+
 
     #Ciudades a crawlear
     start_urls = ['https://www.pisos.com/venta/pisos-{}/fecharecientedesde-desc/1/'.format(city) for city in cities]
 
     should_continue_scraping = {}
-    latest_dates_per_city = {}
+    latest_dates_per_city_db = {}
+    latest_dates_per_city_current_execution = {}
 
     def __init__(self, update_mode=False, *args, **kwargs):
         self.update_mode = update_mode
-        self.max_page_to_search = 100
+        self.max_page_to_search = 10
         self.client = pymongo.MongoClient("mongodb://localhost:27017/")  # Adjust the connection string if needed
         self.db = self.client["pisos"]  # Change to your database name
         self.last_updated_dates_collection = self.client['pisos']['last_updated_dates']
         for city in self.cities:
             self.should_continue_scraping[city] = True
-            self.latest_dates_per_city[city] = self.get_last_known_date(city)
-
+            self.latest_dates_per_city_db[city] = self.get_last_known_date(city)
+            self.latest_dates_per_city_current_execution[city] = OLD_DATE
 
         super(PisosSpider, self).__init__(*args, **kwargs)
 
     def get_last_known_date(self, city):
         record = self.last_updated_dates_collection.find_one({"city": city})
         if record:
-            return record.get("last_updated_date", "Fecha 15/10/1991")
-        return "Fecha 15/10/1991"  # Return an empty string if no record is found
+            return record.get("last_updated_date", OLD_DATE)
+        return OLD_DATE  # Return an OLD_DATE
 
     def parse(self, response):
         if response.status in range(300, 310):
@@ -52,7 +57,7 @@ class PisosSpider(scrapy.Spider):
 
         for ad in response.css('div.ad-preview'):
             id = ad.xpath('@id').get()
-            if not collection.find_one({"id": id}):  # Use the city's specific collection
+            if not collection.find_one({"id": id}):  # Use the city's specific collection # to remove because we want to add updated ads even if they're duplicated
                 data = {
                     'id': id,
                     'title': ad.css('a.ad-preview__title::text').get(),
@@ -73,8 +78,6 @@ class PisosSpider(scrapy.Spider):
         if self.should_continue_scraping[city] and response.url != current_city_homepage and next_page_number <= self.max_page_to_search:
             yield scrapy.Request(next_page_url, callback=self.parse)
 
-
-
     def parse_detail(self, response):
 
         data = response.meta['data']  # Get the previously extracted data passed as meta
@@ -82,28 +85,40 @@ class PisosSpider(scrapy.Spider):
         collection = self.db[city]  # Use the city as the collection name
 
         # Extracting the updated date
-        updated_date = response.css('div.updated-date::text').get().strip() or "Fecha 15/10/1991"
-        current_last_known_date = self.latest_dates_per_city.get(city, "Fecha 15/10/1991")
+        updated_date = response.css('div.updated-date::text').get().strip() or OLD_DATE
 
-        print(updated_date, current_last_known_date)
-        is_more_recent, is_equal_or_more_recent = self.more_recent(updated_date, current_last_known_date)
-        print("is more_recent", is_more_recent, is_equal_or_more_recent)
+        current_last_known_date = self.latest_dates_per_city_db.get(city, OLD_DATE)
 
-        if is_more_recent:
-            # Update the dictionary value
-            self.latest_dates_per_city[city] = updated_date
-            self.last_updated_dates_collection.update_one(
-                {"city": city},
-                {"$set": {"last_updated_date": self.latest_dates_per_city[city]}},
-                upsert=True  # If the city doesn't exist, it will create a new entry
-            )
+        print("update_date ->", updated_date, current_last_known_date)
+        is_more_recent_than_db, is_equal_than_db = self.more_recent(updated_date, current_last_known_date)
+        print("is more_recent -> ", is_more_recent_than_db, "is equal -> ", is_equal_than_db)
 
-        if self.update_mode and not is_equal_or_more_recent:
-            self.should_continue_scraping[city] = False
-            return
+        if self.update_mode:
+            if not (is_equal_than_db or is_more_recent_than_db):
+                self.should_continue_scraping[city] = False
+                return
+            elif is_equal_than_db and collection.find_one({"id": data['id']}):
+                #when we are in the same day that last update, this will prevent to add ads that were already added
+                #this is because update_mode search until 1 day before last update
+                #for example last update was 07/08/2023 but we do not know what time so we will scrap until the first 06/08/2023 flat,
+                #therefore for 07/08/2023 (is_equal) we have to check if the id is in the colection already
+                return
 
+            current_execution_last_date = self.latest_dates_per_city_current_execution.get(city, OLD_DATE)
+            is_more_recent_than_current, _ = self.more_recent(updated_date, current_execution_last_date)
+            if is_more_recent_than_current:
+                self.latest_dates_per_city_current_execution[city] = updated_date
+                if is_more_recent_than_db:
+                    self.update_date_in_db(self.last_updated_dates_collection, city, updated_date)
+                    #do not update the dictionary since we want to retain the original date of the database to not surpass it
+        else:
+            # when not in update mode
+            # if the updated_date is more_recent_than_db update the db and the dictionary
+            if is_more_recent_than_db:
+                self.update_date_in_db(self.last_updated_dates_collection, city, updated_date)
+                self.latest_dates_per_city_db[city] = updated_date
 
-        data['updated_date'] = updated_date  # Removing any extra whitespace
+        data['updated_date'] = updated_date
 
         # Loop through each characteristic item
         for charblock in response.css('ul.charblock-list.charblock-basics > li.charblock-element'):
@@ -154,12 +169,19 @@ class PisosSpider(scrapy.Spider):
 
         print("saved data...", data)
 
+    def update_date_in_db(self, collection, city, updated_date):
+        collection.update_one(
+            {"city": city},
+            {"$set": {"last_updated_date": updated_date}},
+            upsert=True  # If the city doesn't exist, it will create a new entry
+        )
+
     def more_recent(self, date_str1, date_str2):
         # Assuming the format is "Actualizado el DD/MM/YYYY"
         date_format = "%d/%m/%Y"
         date1 = datetime.datetime.strptime(date_str1.split()[-1], date_format)
         date2 = datetime.datetime.strptime(date_str2.split()[-1], date_format)
-        return date1 > date2, date1 >= date2
+        return date1 > date2, date1 == date2
 
     def close_spider(self, spider):
         self.client.close()
