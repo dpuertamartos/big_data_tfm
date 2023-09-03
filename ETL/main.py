aaffa3f1bc7b05c71cb78de543be06b2fdc0464d
David Puerta Martos <dpuerta@usal.es>
2023-09-03 18:19:53 +0200

    now you can change sqllite uri and mongo uri/db with arguments

--- ETL/main.py
+++ ETL/main.py
@@ -2,14 +2,15 @@ from extraction import mongodbHandler
 from transformation import transform_all_data
 from loading import initialize_sql, load_data_to_sql
 import logging
+import argparse
 
 
-def main():
+def main(sql_uri="./pisos.db", mongo_uri="mongodb://localhost:27017/", mongo_db="pisos"):
 
     logging.basicConfig(level=logging.INFO)
 
-    conn, cursor = initialize_sql("pisos.db")
-    mongo_handler = mongodbHandler(mongouri="mongodb://localhost:27017/", database="pisos", sql_cursor=cursor)
+    conn, cursor = initialize_sql(sql_uri)
+    mongo_handler = mongodbHandler(mongouri=mongo_uri, database=mongo_db, sql_cursor=cursor)
 
     mongo_handler.extract_data()
 
@@ -19,4 +20,11 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='ETL script for processing data.')
+    parser.add_argument('--sql_uri', default='./pisos.db', help='SQL database URI.')
+    parser.add_argument('--mongo_uri', default='mongodb://localhost:27017/', help='MongoDB URI.')
+    parser.add_argument('--mongo_db', default='pisos', help='MongoDB database name.')
+
+    args = parser.parse_args()
+
+    main(sql_uri=args.sql_uri, mongo_uri=args.mongo_uri, mongo_db=args.mongo_db)
