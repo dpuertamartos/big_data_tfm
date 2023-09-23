0b14f50587d6f675974c67aad0e6c6db2adbea86
David Puerta Martos <dpuerta@usal.es>
2023-09-23 11:10:34 +0200

    added airflow as orchestrator

---
+++ airflow/dags/ingestion_dag.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'ingestion_dag',
+    default_args=default_args,
+    description='Run ingestion_script.sh at 10:00 and 22:00 daily',
+    schedule_interval='0 10,22 * * *',  # Set to run at both 10:00 and 22:00
+    start_date=datetime(2023, 9, 23),
+    catchup=False,
+)
+
+run_script = BashOperator(
+    task_id='run_ingestion_script',
+    bash_command='/home/ubuntu/big_data_tfm/ingestion_scrapper/ingestion_scrapper/ingestion_script.sh',
+    dag=dag,
+)
---
+++ airflow/dags/mongo_backup_dag.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'mongo_backup_dag',
+    default_args=default_args,
+    description='Run mongo_backup_script.sh every 3 days at 02:00',
+    schedule_interval='0 2 */3 * *',
+    start_date=datetime(2023, 9, 23),
+    catchup=False,
+)
+
+run_script = BashOperator(
+    task_id='run_mongo_backup',
+    bash_command='/home/ubuntu/big_data_tfm/ingestion_scrapper/mongodb/mongo_backup_script.sh >> /home/ubuntu/Desktop/backup_logs/cron_log_$(date +\%Y\%m\%d\%H\%M\%S).log 2>&1',
+    dag=dag,
+)
