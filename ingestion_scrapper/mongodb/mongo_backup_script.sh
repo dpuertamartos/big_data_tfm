9c40923fe9e06cffe3090e3b0583379a1c235dc4
David Puerta Martos <dpuerta@usal.es>
2023-12-07 18:30:14 +0100

    mongo backup script compresses backup

--- ingestion_scrapper/mongodb/mongo_backup_script.sh
+++ ingestion_scrapper/mongodb/mongo_backup_script.sh
@@ -14,3 +14,19 @@ mongodump --host $HOST --port $PORT --out "$BASE_DIR/$SUBFOLDER"
 # Print message to confirm backup was created
 
 echo "MongoDB backup created at $BASE_DIR/$SUBFOLDER on $(date)"
+
+tar -czvf "$BASE_DIR/$SUBFOLDER.tar.gz" -C "$BASE_DIR" "$SUBFOLDER"
+
+# Check if the tar command was successful
+
+if [ $? -eq 0 ]; then
+
+    # Remove the non-compressed backup folder
+    rm -rf "$BASE_DIR/$SUBFOLDER"
+    echo "Backup compressed and original folder removed successfully."
+
+else
+
+    echo "Error in compressing backup, original folder not removed."
+
+fi
