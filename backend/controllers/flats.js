3d3e1b53fcfd3c4bd44c2c46809071e2009b798c
dpuertamartos <dpuerta@usal.es>
2023-12-10 18:14:23 +0000

    restructured into controllers and utils

---
+++ backend/controllers/flats.js
@@ -0,0 +1,30 @@
+const router = require('express').Router()
+const { sequelize, select } = require('../util/db')
+
+
+router.get('/', async (req, res) => {
+    const flats = await sequelize.query("SELECT * FROM pisos LIMIT 100", { type: select })
+    res.json(flats)
+})
+
+// app.get('/api/flats/:id', (request, response) => {
+//   const id = request.params.id
+//   const flat = flats.find(flat => flat.id === id)
+
+//   if (flat) {
+//     response.json(flat)
+//   } else {
+//     response.status(404).end()
+//   }
+
+//   response.json(flat)
+// })
+
+// app.delete('/api/flats/:id', (request, response) => {
+//   const id = request.params.id
+//   flats = flats.filter(flat => flat.id !== id)
+
+//   response.status(204).end()
+// })
+
+module.exports = router
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors = require('cors')
-require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
 const app = express()
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const { PORT } = require('./util/config') 
+const { connectToDatabase } = require('./util/db') 
+
+const flatsRouter = require('./controllers/flats')
+
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -22,63 +24,16 @@ app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
-
-app.get('/api/flats', async (req, res) => {
-  const flats = await sequelize.query("SELECT * FROM pisos LIMIT 10", { type: QueryTypes.SELECT })
-  res.json(flats)
-})
-
-// const generateId = () => {
-//   const maxId = flats.length > 0
-//     ? Math.max(...flats.map(n => n.id))
-//     : 0
-//   return maxId + 1
-// }
-
-// app.post('/api/flats', (request, response) => {
-//   const body = request.body
-
-//   if (!body.content) {
-//     return response.status(400).json({ 
-//       error: 'content missing' 
-//     })
-//   }
-
-//   const flat = {
-//     content: body.content,
-//     important: body.important || false,
-//     date: new Date(),
-//     id: generateId(),
-//   }
-
-//   flats = flats.concat(flat)
-
-//   response.json(flat)
-// })
-
-// app.get('/api/flats/:id', (request, response) => {
-//   const id = request.params.id
-//   const flat = flats.find(flat => flat.id === id)
-
-//   if (flat) {
-//     response.json(flat)
-//   } else {
-//     response.status(404).end()
-//   }
-
-//   response.json(flat)
-// })
-
-// app.delete('/api/flats/:id', (request, response) => {
-//   const id = request.params.id
-//   flats = flats.filter(flat => flat.id !== id)
-
-//   response.status(204).end()
-// })
+app.use('/api/flats', flatsRouter)
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+const start = async () => {
+    await connectToDatabase()
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`)
+    })
+}
+
+start()
+
