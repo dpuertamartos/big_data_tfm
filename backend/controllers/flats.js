f4be83f896962f47c1ce034abfe32e6bd9017086
dpuertamartos <dpuerta@usal.es>
2023-12-13 12:54:22 +0000

    send error to middleware

--- backend/controllers/flats.js
+++ backend/controllers/flats.js
@@ -2,9 +2,14 @@ const router = require('express').Router()
 const { sequelize, select } = require('../util/db')
 
 
-router.get('/', async (req, res) => {
-    const flats = await sequelize.query('SELECT * FROM pisos LIMIT 100', { type: select })
-    res.json(flats)
+router.get('/', async (req, res, next) => {
+    try {
+        const flats = await sequelize.query('SELECT * FROM pisos LIMIT 100', { type: select })
+        res.json(flats)
+    }
+    catch(exception) {
+        next(exception)
+    }
 })
 
 // app.get('/api/flats/:id', (request, response) => {
