8dc50adcf9927783c7a917570e8fcebbdb757d2d
dpuertamartos <dpuerta@usal.es>
2023-12-18 12:21:16 +0000

    API provides best flats by rating and city

--- backend/controllers/flats.js
+++ backend/controllers/flats.js
@@ -1,30 +1,58 @@
 const router = require('express').Router()
 const { sequelize, select } = require('../util/db')
 
+const getFlats = async (flatNumber) => {
+    return await sequelize.query(
+        `SELECT * 
+         FROM pisos 
+         WHERE active = 1 
+         LIMIT :flatNumber`,
+        {
+            type: select,
+            replacements: { flatNumber: flatNumber }
+        }
+    )
+}
+
+const getBestFlats = async () => {
+    return await sequelize.query(
+        `SELECT * 
+         FROM pisos 
+         WHERE rating IS NOT NULL AND active = 1 
+         ORDER BY rating ASC 
+         LIMIT 20`,
+        { type: select }
+    )
+}
+
+const getBestFlatsByCity = async (city) => {
+    return await sequelize.query(
+        `SELECT * 
+         FROM pisos 
+         WHERE city = :city AND rating IS NOT NULL AND active = 1 
+         ORDER BY rating ASC 
+         LIMIT 20`,
+        {
+            type: select,
+            replacements: { city: city }
+        }
+    )
+}
 
 router.get('/', async (req, res) => {
-    const flats = await sequelize.query('SELECT * FROM pisos LIMIT 100', { type: select })
+    const flats = await getFlats(10)
     res.json(flats)
 })
 
-// app.get('/api/flats/:id', (request, response) => {
-//   const id = request.params.id
-//   const flat = flats.find(flat => flat.id === id)
+router.get('/best_rating', async (req, res) => {
+    const best_flats = await getBestFlats()
+    res.json(best_flats)
+})
 
-//   if (flat) {
-//     response.json(flat)
-//   } else {
-//     response.status(404).end()
-//   }
-
-//   response.json(flat)
-// })
-
-// app.delete('/api/flats/:id', (request, response) => {
-//   const id = request.params.id
-//   flats = flats.filter(flat => flat.id !== id)
-
-//   response.status(204).end()
-// })
+router.get('/best_rating/:city', async (req, res) => {
+    const city = req.params.city
+    const best_flats_per_city = await getBestFlatsByCity(city)
+    res.json(best_flats_per_city)
+})
 
 module.exports = router
--- frontend/src/components/Flat.jsx
+++ frontend/src/components/Flat.jsx
@@ -6,7 +6,7 @@ const Flat = ({ flat }) => {
     return (
       <li>
         <Link to={`/flats/${flat.id}`}>{flat.title}</Link>,
-        Price: {flat.price_euro} 
+        Price: {flat.price_euro}, Rating:{Math.floor(flat.rating*100)/100} 
       </li>
     )
   }
