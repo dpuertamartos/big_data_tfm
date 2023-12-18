9ebe994dc38bc7d603d4c476d275d160b42ecd49
dpuertamartos <dpuerta@usal.es>
2023-12-18 19:34:33 +0000

    flats api receives parameters

--- backend/controllers/flats.js
+++ backend/controllers/flats.js
@@ -1,58 +1,65 @@
 const router = require('express').Router()
 const { sequelize, select } = require('../util/db')
 
-const getFlats = async (flatNumber) => {
-    return await sequelize.query(
-        `SELECT * 
-         FROM pisos 
-         WHERE active = 1 
-         LIMIT :flatNumber`,
-        {
-            type: select,
-            replacements: { flatNumber: flatNumber }
-        }
-    )
+const getFlats = async (options) => {
+    // Destructuring with default values
+    const {
+        columns = '*',
+        limitNumber = 10,
+        orderBy = 'id ASC', // Default ordering
+        active = 1,
+        city = null,
+        rating = null
+    } = options
+
+    // Start building the query
+    let query = `SELECT ${columns} FROM pisos WHERE active = :active`
+
+    // Building replacements object
+    let replacements = {
+        active: active,
+        limitNumber: limitNumber,
+    }
+
+    if (city) {
+        query += ` AND city = :city`
+        replacements.city = city
+    }
+
+    if (rating !== null) {
+        query += ` AND rating IS NOT NULL`
+    }
+
+    query += ` ORDER BY ${orderBy} LIMIT :limitNumber`
+
+    return await sequelize.query(query, {
+        type: select,
+        replacements: replacements
+    })
 }
 
-const getBestFlats = async () => {
-    return await sequelize.query(
-        `SELECT * 
-         FROM pisos 
-         WHERE rating IS NOT NULL AND active = 1 
-         ORDER BY rating ASC 
-         LIMIT 20`,
-        { type: select }
-    )
-}
-
-const getBestFlatsByCity = async (city) => {
-    return await sequelize.query(
-        `SELECT * 
-         FROM pisos 
-         WHERE city = :city AND rating IS NOT NULL AND active = 1 
-         ORDER BY rating ASC 
-         LIMIT 20`,
-        {
-            type: select,
-            replacements: { city: city }
-        }
-    )
-}
 
 router.get('/', async (req, res) => {
-    const flats = await getFlats(10)
+    const flats = await getFlats({ limitNumber: 10 })
     res.json(flats)
 })
 
-router.get('/best_rating', async (req, res) => {
-    const best_flats = await getBestFlats()
-    res.json(best_flats)
-})
+router.get('/rating', async (req, res) => {
+    const { city, minPrice, maxPrice, limitNumber, sorting } = req.query
 
-router.get('/best_rating/:city', async (req, res) => {
-    const city = req.params.city
-    const best_flats_per_city = await getBestFlatsByCity(city)
-    res.json(best_flats_per_city)
+    let options = {}
+
+    let sort = 'ASC'
+    if (sorting) {
+        sort = sorting
+    }
+    if (city) options.city = city
+    if (minPrice) options.minPrice = minPrice
+    if (maxPrice) options.maxPrice = maxPrice
+    if (limitNumber) options.limitNumber = limitNumber
+
+    const best_flats = await getFlats({ ...options, orderBy: 'rating ' + sort, limitNumber: 10 })
+    res.json(best_flats)
 })
 
 module.exports = router
