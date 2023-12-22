c634c6bfa14587ef3cb28c370cf13fffcb1b998b
David Puerta Martos <dpuerta@usal.es>
2023-12-22 18:34:59 +0100

    airflow dag now decides to run checking_deletes_task once per day

--- airflow/dags/ingestion_and_ETL_dag.py
+++ airflow/dags/ingestion_and_ETL_dag.py
@@ -1,6 +1,15 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import BranchPythonOperator
+from airflow.utils.dates import days_ago
+
+def _choose_task_to_run():
+    current_hour = datetime.now().hour
+    if current_hour < 12:  # Assuming 10:00 run is the first run of the day
+        return 'run_checking_deletes_task'
+    else:
+        return 'run_transformation_script'
 
 default_args = {
     'owner': 'airflow',
@@ -27,6 +36,20 @@ ingestion_task = BashOperator(
     dag=dag,
 )
 
+# Branching Task
+branch_task = BranchPythonOperator(
+    task_id='branch_task',
+    python_callable=_choose_task_to_run,
+    dag=dag,
+)
+
+# Checking Deletes Task (run only once per day)
+checking_deletes_task = BashOperator(
+    task_id='run_checking_deletes_task',
+    bash_command="/home/ubuntu/big_data_tfm/ingestion_scrapper/ingestion_scrapper/ad_up_checking_script.sh ",
+    dag=dag,
+)
+
 # Transformation Task
 transformation_task = BashOperator(
     task_id='run_transformation_script',
@@ -35,4 +58,7 @@ transformation_task = BashOperator(
 )
 
 # Setting Task Dependencies
-ingestion_task >> transformation_task  # Run transformation_task after ingestion_task completes
+ingestion_task >> branch_task
+branch_task >> checking_deletes_task >> transformation_task
+branch_task >> transformation_task
+
