eb824c77485659658a41660a12cfc258960f1389
David Puerta Martos <dpuerta@usal.es>
2023-12-22 18:26:39 +0100

    checks only 2 days of the month in each run depending on current execution time

--- ingestion_scrapper/ingestion_scrapper/spiders/check_up_spider.py
+++ ingestion_scrapper/ingestion_scrapper/spiders/check_up_spider.py
@@ -1,28 +1,43 @@
 import scrapy
 from pymongo import MongoClient
 from scrapy.http import Request
-from datetime import datetime
+from datetime import datetime, timedelta
+import time
 
 class AdUpCheckingSpider(scrapy.Spider):
     name = 'ad_up_checking'
 
-    def __init__(self):
+    def __init__(self, request_limit=0, pause_time=120):
         # Establish MongoDB connection
         self.client = MongoClient("mongodb://localhost:27017/")
         self.db = self.client['pisos']
         self.number_of_flat_checked = 0
         self.number_of_flat_retired = 0
+        self.request_limit = int(request_limit)
+        self.pause_time = int(pause_time)
 
     def start_requests(self):
         excluded_collections = ['last_updated_dates', 'amount parsed']
         collections = [col for col in self.db.list_collection_names() if col not in excluded_collections]
+        today = datetime.now()
+        check_days = [(today + timedelta(days=1)).day, (today - timedelta(days=14)).day]
 
         for collection in collections:
-            print(f"checking collection {collection}")
-            for doc in self.db[collection].find({"active": True}):
-                link = doc.get('link')
-                if link:
-                    yield Request(link, self.check_url, meta={'original_link': link, 'doc_id': doc['_id'], 'collection': collection})
+            for day in check_days:
+                print(f"checking collection {collection}, for day {day}")
+                pipeline = [
+                    {"$match": {"active": True}},
+                    {"$project": {"dayOfMonth": {"$dayOfMonth": "$updatedAt"}, "doc": "$$ROOT"}},
+                    {"$match": {"dayOfMonth": day}}
+                ]
+                cursor = self.db[collection].aggregate(pipeline)
+                for doc in cursor:
+                    link = doc['doc'].get('link')
+                    if link:
+                        yield Request(link, self.check_url, meta={'original_link': link, 'doc_id': doc['doc']['_id'], 'collection': collection})
+                        self.number_of_flat_checked += 1
+                        if self.request_limit > 0 and self.number_of_flat_checked % self.request_limit == 0:
+                            time.sleep(self.pause_time)
 
     def check_url(self, response):
         original_link = response.meta['original_link']
@@ -41,7 +56,7 @@ class AdUpCheckingSpider(scrapy.Spider):
             )
             self.number_of_flat_retired += 1
 
-        self.number_of_flat_checked += 1
+
 
     def closed(self, reason):
         # Close MongoDB connection when spider is closed
