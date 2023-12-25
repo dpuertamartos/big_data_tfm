ff1e7c35fad9c14b5c370aa6dd59e83a28cb49c7
David Puerta Martos <dpuerta@usal.es>
2023-12-25 13:38:20 +0100

    predictions are now made in batch for each model

--- data_analysis/generate_predictions.py
+++ data_analysis/generate_predictions.py
@@ -2,32 +2,66 @@
 import pandas as pd
 import joblib
 from data_cleaning import clean_data  # Assuming this is your data cleaning module
+import sqlite3
+import argparse
+import time
+
 
 def load_model(model_path):
     # Loads both the model and its preprocessing pipeline
     model, preprocessor = joblib.load(model_path)
     return model, preprocessor
 
+
 def make_predictions(df, models):
-    predictions = []
+    # Initialize an empty Series to store predictions
+    predictions = pd.Series(index=df.index)
 
-    for index, row in df.iterrows():
-        row_df = row.to_frame().T
-
-        city = row['city']
-        price_category = 'cheap' if row['price_euro'] <= 350000 else 'expensive'
+    # Group by city and price category
+    for (city, price_category), group_df in df.groupby(['city', df['price_euro'] <= 350000]):
+        price_category = 'cheap' if price_category else 'expensive'
 
         # Select the appropriate model and its pipeline
         model, preprocessor = models[city][price_category]
 
-        # Preprocess the row
-        processed_row = preprocessor.transform(row_df)
+        # Preprocess the group
+        processed_group = preprocessor.transform(group_df)
 
-        # Generate prediction
-        prediction = model.predict(processed_row)
-        predictions.append(prediction[0])
+        # Generate predictions for the group
+        group_predictions = model.predict(processed_group)
+
+        # Assign predictions back to the corresponding indices in the original DataFrame
+        predictions[group_df.index] = group_predictions
 
     return predictions
+
+
+# def store_predictions_in_db(database, df):
+#     with sqlite3.connect(database) as conn:
+#         df.to_sql('predicted', conn, if_exists='replace', index=False)
+#     print("Predictions stored in 'predicted' table.")
+#
+#
+# def predict_and_store_all(database, models):
+#     with sqlite3.connect(database) as conn:
+#         df = pd.read_sql_query("SELECT * FROM pisos WHERE active = 1", conn)
+#
+#     df_cleaned = clean_data(df)
+#     df_cleaned['predictions'] = make_predictions(df_cleaned, models)
+#     store_predictions_in_db(database, df_cleaned[['id', 'predictions']])
+#
+#
+# def predict_and_store_new_entries(database, models, days=1):
+#
+#     days_ago_unix_time = time.time() - (days * 24 * 60 * 60)
+#     with sqlite3.connect(database) as conn:
+#         query = f"SELECT * FROM pisos WHERE updatedat > {days_ago_unix_time}"
+#         new_entries = pd.read_sql_query(query, conn)
+#
+#     new_cleaned = clean_data(new_entries)
+#     new_cleaned['predictions'] = make_predictions(new_cleaned, models)
+#     store_predictions_in_db(database, new_cleaned[['id', 'predictions']])
+
 
 def main():
     df = pd.read_csv("pisos.csv")
