9b188a7def71acaea84df64243c40fc073164987
David Puerta Martos <dpuerta@usal.es>
2023-12-27 17:16:11 +0100

    aggregation is performed in all combinations of selected groups

--- ETL/secondary/dw_extraction.py
+++ ETL/secondary/dw_extraction.py
@@ -1,6 +1,49 @@
 import pandas as pd
 import sqlite3
+import itertools
 
+
+def return_all_combinations(array):
+    all_combinations = []
+
+    for L in range(1, len(array) + 1):
+        for subset in itertools.combinations(array, L):
+            all_combinations.append(list(subset))
+
+    return all_combinations
+
+
+def rename_col_to_group(col):
+    return f'{col}_group'
+
+
+def reindex(df, groups):
+    groups_new_names = [rename_col_to_group(element) for element in groups]
+    print(groups_new_names)
+    if len(groups_new_names) > 1:
+        for i, name in enumerate(groups_new_names):
+            df.index = df.index.set_names(name, level=i)
+    elif len(groups_new_names) == 1:
+        df.index = df.index.set_names(groups_new_names[0], level=None)
+    return df.reset_index()
+
+
+# Aggregation function
+def aggregate_data(df, numeric_cols, categorical_cols, groups_to_organize_by, all_groups_possibilities):
+    agg_funcs = {col: 'mean' for col in numeric_cols}
+    agg_funcs.update({col: 'count' for col in categorical_cols})
+
+    grouped_df = df.groupby(groups_to_organize_by).agg(agg_funcs)
+    grouped_df = reindex(grouped_df, groups_to_organize_by)
+
+    #this will set the value to 'all' if we are not grouping by that group
+    for element in all_groups_possibilities:
+        if element not in groups_to_organize_by:
+            grouped_df[rename_col_to_group(element)] = 'all'
+
+    return grouped_df
+
+# START EXTRACTION
 # Database connection (modify the path to your SQLite database file)
 conn = sqlite3.connect('../../data_analysis/pisos_backup.db')
 
@@ -8,46 +51,32 @@ conn = sqlite3.connect('../../data_analysis/pisos_backup.db')
 query = 'SELECT * FROM pisos'
 df = pd.read_sql(query, conn)
 
-# Convert 'updatedat' from Unix time to datetime
-df['updated_month'] = pd.to_datetime(df['updatedat'], unit='s').dt.to_period('M')
+# Convert 'createdat' from Unix time to datetime
+df['updated_month'] = pd.to_datetime(df['createdat'], unit='s').dt.to_period('M')
 
 # Define the columns to be aggregated
 numeric_columns = ['price_euro', 'superficie_construida_m2', 'superficie_util_m2', 'superficie_solar_m2', 'habitaciones', 'banos']
 categorical_columns = ['city', 'type']  # Add more columns if needed
 
-
-def reindex(df, groups):
-    groups_new_names = [f'{element}_group' for element in groups]
-    df.index.rename(groups_new_names, inplace=True)
-    return df.reset_index()
-
-
-# Aggregation function
-def aggregate_data(df, numeric_cols, categorical_cols, groups):
-    agg_funcs = {col: 'mean' for col in numeric_cols}
-    agg_funcs.update({col: 'count' for col in categorical_cols})
-
-    grouped_df = df.groupby(groups).agg(agg_funcs)
-    grouped_df = reindex(grouped_df, groups)
-
-    return grouped_df
-
-
 # Aggregate data for all records
-agg_all_df = aggregate_data(df, numeric_columns, categorical_columns, groups=['updated_month', 'city', 'type'])
-# Aggregate data only for active records
-agg_active_df = aggregate_data(df[df['active'] == 1], numeric_columns, categorical_columns, groups=['city', 'type'])
+
+# START TRANSFORMATION
+group_variables = ['updated_month', 'city', 'type', 'active']
+all_combinations = return_all_combinations(group_variables)
+all_aggregated_data = [aggregate_data(df, numeric_columns, categorical_columns, groups, group_variables) for groups in all_combinations]
 
 # Concatenate the results
-final_df = pd.concat([agg_all_df, agg_active_df])
+final_df = pd.concat(all_aggregated_data)
 
 final_df['updated_month_group'] = final_df['updated_month_group'].astype(str)
 
 print(final_df)
+
+# START LOADING
+
 # Insert aggregated data into 'pisos_dw'
 final_df.to_sql('pisos_dw', conn, if_exists='replace', index=False)
 
 # Close the database connection
 conn.close()
-
 print("Data aggregation and insertion completed successfully.")
