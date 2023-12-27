153e23ad271046e5fa91db7799055695823526e8
David Puerta Martos <dpuerta@usal.es>
2023-12-27 14:51:13 +0100

    generalized reindexing

--- ETL/secondary/dw_extraction.py
+++ ETL/secondary/dw_extraction.py
@@ -15,6 +15,13 @@ df['updated_month'] = pd.to_datetime(df['updatedat'], unit='s').dt.to_period('M'
 numeric_columns = ['price_euro', 'superficie_construida_m2', 'superficie_util_m2', 'superficie_solar_m2', 'habitaciones', 'banos']
 categorical_columns = ['city', 'type']  # Add more columns if needed
 
+
+def reindex(df, groups):
+    groups_new_names = [f'{element}_group' for element in groups]
+    df.index.rename(groups_new_names, inplace=True)
+    return df.reset_index()
+
+
 # Aggregation function
 def aggregate_data(df, numeric_cols, categorical_cols, group_by_month=True):
     agg_funcs = {col: 'mean' for col in numeric_cols}
@@ -22,20 +29,20 @@ def aggregate_data(df, numeric_cols, categorical_cols, group_by_month=True):
 
     if group_by_month:
         # Group by month, city, and type
-        grouped_df = df.groupby(['updated_month', 'city', 'type']).agg(agg_funcs)
-        grouped_df.index.rename(['month', 'city_group', 'type_group'], inplace=True)
-        grouped_df = grouped_df.reset_index()
-        grouped_df['category'] = 'all'
+        groups = ['updated_month', 'city', 'type']
+        categ = 'all'
+
     else:
         # Group only by city and type for active records
-        grouped_df = df.groupby(['city', 'type']).agg(agg_funcs)
-        grouped_df.index.rename(['city_group', 'type_group'], inplace=True)
-        grouped_df = grouped_df.reset_index()
-        grouped_df['category'] = 'active'
+        groups = ['city', 'type']
+        categ = 'active'
 
-
+    grouped_df = df.groupby(groups).agg(agg_funcs)
+    grouped_df = reindex(grouped_df, groups)
+    grouped_df['category'] = categ
 
     return grouped_df
+
 
 # Aggregate data for all records
 agg_all_df = aggregate_data(df, numeric_columns, categorical_columns, group_by_month=True)
@@ -46,8 +53,9 @@ agg_active_df = aggregate_data(df[df['active'] == 1], numeric_columns, categoric
 # Concatenate the results
 final_df = pd.concat([agg_all_df, agg_active_df])
 
-final_df['month'] = final_df['month'].astype(str)
+final_df['updated_month_group'] = final_df['updated_month_group'].astype(str)
 
+print(final_df)
 # Insert aggregated data into 'pisos_dw'
 final_df.to_sql('pisos_dw', conn, if_exists='replace', index=False)
 
