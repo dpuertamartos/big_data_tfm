31b637666034242533d6b53e2a858c0175feccf0
David Puerta Martos <dpuerta@usal.es>
2024-01-01 22:33:59 +0100

    adapted to docker containers

--- airflow/dags/mongo_backup_dag.py
+++ airflow/dags/mongo_backup_dag.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+from airflow.operators.docker_operator import DockerOperator
+from docker.types import Mount
 from config import project
 
 default_args = {
@@ -15,14 +16,25 @@ default_args = {
 dag = DAG(
     'mongo_backup_dag',
     default_args=default_args,
-    description='Run mongo_backup_script.sh every 3 days at 02:00',
+    description='Backup MongoDB every 3 days',
     schedule_interval='0 2 */3 * *',
     start_date=datetime(2023, 9, 23),
     catchup=False,
 )
 
-run_script = BashOperator(
-    task_id='run_mongo_backup',
-    bash_command='/home/ubuntu/big_data_tfm/ingestion_scrapper/mongodb/mongo_backup_script.sh >> /home/ubuntu/Desktop/backup_logs/cron_log_$(date +\%Y\%m\%d\%H\%M\%S).log 2>&1',
+mongo_backup_task = DockerOperator(
+    task_id='mongo_backup',
+    image='mongo:latest',  # Imagen de MongoDB
+    api_version='auto',
+    auto_remove=True,
+    docker_url='unix://var/run/docker.sock',
+    mounts=[
+        Mount(source=f"{project}_mongodb-data", target="/data/db", type="volume"),
+        Mount(source=f"{project}_mongodb-backups", target="/backups", type="volume"),
+    ],
+    command='sh -c "mongodump --host mongodb-container --port 27017 --out=/tmp/mongo_backup && tar -czvf /backups/mongo_backup_$(date +\\"%Y%m%d\\").tar.gz -C /tmp mongo_backup && rm -rf /tmp/mongo_backup"',  # Comando de backup
+    network_mode=f'{project}_custom-network',
     dag=dag,
 )
+
+mongo_backup_task
