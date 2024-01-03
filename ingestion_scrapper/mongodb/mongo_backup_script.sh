6a945324a9b86c16d6e0a90dfbfbccba2d47e30e
dpuertamartos <dpuerta@usal.es>
2024-01-03 21:39:07 +0000

    updated mongo backup script

--- ingestion_scrapper/mongodb/mongo_backup_script.sh
+++ ingestion_scrapper/mongodb/mongo_backup_script.sh
@@ -1,32 +1,7 @@
 #!/bin/bash
+BACKUPDIRECTORY=/home/ubuntu/Desktop/backups_mongo
+# Obtener la fecha actual en el formato Año_Mes_Día
+CURRENT_DATE=$(date +%Y_%m_%d)
 
-# Set variables
-HOST="localhost"
-PORT="27017"
-BASE_DIR="/home/ubuntu/Desktop/backups_mongo/"
-# Generate the subfolder name in the format YYYYMMDD
-
-SUBFOLDER=$(date +"%Y%m%d")
-
-# Run mongodump command and save to the subfolder
-
-mongodump --host $HOST --port $PORT --out "$BASE_DIR/$SUBFOLDER"
-# Print message to confirm backup was created
-
-echo "MongoDB backup created at $BASE_DIR/$SUBFOLDER on $(date)"
-
-tar -czvf "$BASE_DIR/$SUBFOLDER.tar.gz" -C "$BASE_DIR" "$SUBFOLDER"
-
-# Check if the tar command was successful
-
-if [ $? -eq 0 ]; then
-
-    # Remove the non-compressed backup folder
-    rm -rf "$BASE_DIR/$SUBFOLDER"
-    echo "Backup compressed and original folder removed successfully."
-
-else
-
-    echo "Error in compressing backup, original folder not removed."
-
-fi
+# Ejecutar el comando de backup con la fecha en el nombre del archivo
+docker run --rm -v big_data_tfm_mongodb-data:/data -v $BACKUPDIRECTORY:/backup ubuntu tar czvf /backup/mongodb-volume-backup_$CURRENT_DATE.tar.gz -C /data .
