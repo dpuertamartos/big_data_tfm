d6aea6468cb5e2c8fbce87630b9e18d48c7162af
David Puerta Martos <dpuerta@usal.es>
2024-01-18 02:29:36 +0100

    LineGraph dinamically adapts to the number of y axis selected

--- frontend/src/components/LineGraph.jsx
+++ frontend/src/components/LineGraph.jsx
@@ -2,97 +2,96 @@ import React, { useState } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
-
-
-
-const LineGraph = ({selectedCities, data, activeDotSelector, yAxisOptions, yAxisDefault}) => {
-  const [yAxisKey, setYAxisKey] = useState([yAxisDefault]); // Default to the first option
+const LineGraph = ({ selectedCities, data, activeDotSelector, yAxisOptions, yAxisDefault }) => {
+  const [selectedYAxisKeys, setSelectedYAxisKeys] = useState([yAxisDefault]);
 
   // Transform the data to the required format
-  const transformData = (rawData) => {
-    const monthMap = {}
+  const transformData = (rawData, yAxisKey) => {
+    const monthMap = {};
 
     rawData.forEach(item => {
       const month = item.updated_month_group;
-      // Skip the entry if the month is 'all'
-      if (month === 'all') {
-        return
-      }
+      if (month === 'all') return;
 
       if (!monthMap[month]) {
-        monthMap[month] = { name: month }
+        monthMap[month] = { name: month };
       }
       monthMap[month][item.city_group] = item[yAxisKey];
-      //monthMap[month][item.city_group] = item.price_euro_mean_excluding_outliers;
-    })
+    });
 
-    return Object.values(monthMap)
-  }
-
-  const transformedData = transformData(data)
+    return Object.values(monthMap);
+  };
 
   // Function to generate a unique stroke color for each line
   const getStrokeColor = (city) => {
-    // A simple hash function to convert a string to a color
     const hashStringToColor = (str) => {
-      let hash = 0
+      let hash = 0;
       for (let i = 0; i < str.length; i++) {
-        hash = str.charCodeAt(i) + ((hash << 5) - hash)
-        hash = hash & hash // Convert to 32bit integer
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+        hash = hash & hash;
       }
-      let color = '#'
+      let color = '#';
       for (let i = 0; i < 3; i++) {
-        const value = (hash >> (i * 8)) & 255
+        const value = (hash >> (i * 8)) & 255;
         color += ('00' + value.toString(16)).substr(-2);
       }
-      return color
+      return color;
     };
-  
     return hashStringToColor(city);
   };
-  
+
   const handleYAxisChange = (event) => {
-    setYAxisKey(event.target.value);
+    const newKeys = event.target.value;
+    if (newKeys.length <= 3) { // Limit to a maximum of 3 yAxisKeys
+      setSelectedYAxisKeys(newKeys);
+    }
   };
+
+  // Calculate the width for each chart based on the number of selected yAxisKeys
+  const chartWidth = selectedYAxisKeys.length === 1 ? '100%' : `${100 / selectedYAxisKeys.length}%`;
 
   return (
     <div>
       <FormControl style={{ minWidth: 120, margin: '20px' }}>
-        <InputLabel id="y-axis-select-label">Y-Axis Key</InputLabel>
+        <InputLabel id="y-axis-select-label">Y-Axis Keys</InputLabel>
         <Select
           labelId="y-axis-select-label"
           id="y-axis-select"
-          value={yAxisKey}
+          multiple
+          value={selectedYAxisKeys}
           onChange={handleYAxisChange}
+          renderValue={(selected) => selected.join(', ')}
         >
           {yAxisOptions.map((option) => (
             <MenuItem key={option} value={option}>{option}</MenuItem>
           ))}
         </Select>
       </FormControl>
-      <ResponsiveContainer width="100%" height={500}>
-        <LineChart
-          width={500}
-          height={300}
-          data={transformedData}
-          margin={{ top: 40, right: 30, left: 20, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          {selectedCities.map(city => (
-            <Line 
-              key={city}
-              type="monotone" 
-              dataKey={city} 
-              stroke={getStrokeColor(city)} 
-              activeDot={city === activeDotSelector ? { r: 8 } : null}
-            />
-          ))} 
-        </LineChart>
-      </ResponsiveContainer>
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' }}>
+        {selectedYAxisKeys.map((yAxisKey, index) => (
+          <ResponsiveContainer key={index} width={chartWidth} height={500}>
+            <LineChart
+              data={transformData(data, yAxisKey)}
+              margin={{ top: 40, right: 30, left: 20, bottom: 5 }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              {selectedCities.map(city => (
+                <Line 
+                  key={city}
+                  type="monotone" 
+                  dataKey={city} 
+                  stroke={getStrokeColor(city)} 
+                  activeDot={city === activeDotSelector ? { r: 8 } : null}
+                />
+              ))} 
+            </LineChart>
+          </ResponsiveContainer>
+        ))}
+      </div>
     </div>
   );
 };
