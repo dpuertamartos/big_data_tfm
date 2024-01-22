9aa8d10713a4796b4277418d1288addbb4bf9bd7
David Puerta Martos <dpuerta@usal.es>
2024-01-22 23:53:20 +0100

    added tools button in appbar when size of screen is small

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,35 +5,48 @@ import FlatDetailed from './components/FlatDetailed';
 import Flats from './components/Flats';
 import Trends from './components/Trends';
 import Footer from './components/Footer';
-import { AppBar, Toolbar, Button, CssBaseline, Container } from '@mui/material';
+import { AppBar, Toolbar, Button, CssBaseline, Container, IconButton, useTheme, useMediaQuery, Box } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const App = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const theme = useTheme();
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up('md'));
   const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
 
   return (
     <>
       <CssBaseline />
       <AppBar position="fixed">
-        <Toolbar>
-          <Button color="inherit" component={Link} to="/">
-            Home
-          </Button>
-          <Button color="inherit" component={Link} to="/flats">
-            Explore
-          </Button>
-          <Button color="inherit" component={Link} to="/trends">
-            Trends
-          </Button>
-          <Button color="inherit" component={Link} to="/">
-            Contact
-          </Button>                         
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          <Box sx={{ display: 'flex', flexGrow: 1 }}>
+            <Button color="inherit" component={Link} to="/">Home</Button>
+            <Button color="inherit" component={Link} to="/flats">Explore</Button>
+            <Button color="inherit" component={Link} to="/trends">Trends</Button>
+            <Button color="inherit" component={Link} to="/">Contact</Button>
+          </Box>
+          {!isLargeScreen && (
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="end"
+              onClick={handleDrawerToggle}
+              sx={{ fontSize: '1.5rem' }} // Larger icon size
+            >
+              <SettingsIcon /> 
+            </IconButton>
+          )}
         </Toolbar>
       </AppBar>
       <Toolbar />
       <Container>
         <Routes>
           <Route path="/flats/:id" element={<FlatDetailed />} />
-          <Route path="/flats" element={<Flats errorMessage={errorMessage} />} />
+          <Route path="/flats" element={<Flats errorMessage={errorMessage} drawerOpen={drawerOpen} handleDrawerToggle={handleDrawerToggle} />} />
           <Route path="/trends" element={<Trends />} />
           <Route path="/" element={<Home />} />
         </Routes>
